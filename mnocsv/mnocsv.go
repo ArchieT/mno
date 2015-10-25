5200db3256ec08ab694e0d0c81adf8af02a4dac4
Michał Krzysztof Feiler <feiler.michal@mensa.org.pl>
2015-10-25 16:15:12 +0100

    Working on mnocsv

--- mnocsv/mnocsv.go
+++ mnocsv/mnocsv.go
@@ -2,20 +2,40 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"os"
 	"github.com/ArchieT/mno/wyniki"
 )
 
-func main() {
-	var data, miejsce string
-	var czas, czasplus int
+var data, miejsce string
+var czas, czasplus int
+var filename string
 
+func main() {
 	flag.StringVar(&data, "d", "", "data")
 	flag.StringVar(&miejsce, "m", "", "miejsce")
 
 	flag.IntVar(&czas, "t", 0, "czas podstawowy")
 	flag.IntVar(&czasplus, "p", 0, "czas nisko naliczany")
 
-	filename := flag.Args()[0]
+	flag.StringVar(&filename, "f", "", "nazwa pliku csv")
 
+	flag.Parse()
+
+	file,err := os.Open(filename)
+	if err!=nil {
+		panic(err)
+	}
+
+	w := wyniki.Daj(file)
+
+	var z wyniki.Zawody
+	z.Czas = czas
+	z.CzasPlus = czasplus
+	z.Data = data
+	z.Miejsce = miejsce
+	z.Wyniki = &w
+
+	write := os.Stdout
+
+	z.Present(write)
 }
