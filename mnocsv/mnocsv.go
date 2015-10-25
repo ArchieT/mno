071b322cccd19a146d8d6aba8c04a8a465b62b6d
Michał Krzysztof Feiler <feiler.michal@mensa.org.pl>
2015-10-25 15:42:09 +0100

    gofmt mnocsv

--- mnocsv/mnocsv.go
+++ mnocsv/mnocsv.go
@@ -1,22 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"flag"
+	"fmt"
 	"github.com/ArchieT/mno/wyniki"
 )
 
 func main() {
-	var data,miejsce string
-	var czas,czasplus int
+	var data, miejsce string
+	var czas, czasplus int
 
-	flag.StringVar(&data,"d","","data")
-	flag.StringVar(&miejsce,"m","","miejsce")
+	flag.StringVar(&data, "d", "", "data")
+	flag.StringVar(&miejsce, "m", "", "miejsce")
 
-	flag.IntVar(&czas,"t",0,"czas podstawowy")
-	flag.IntVar(&czasplus,"p",0,"czas nisko naliczany")
+	flag.IntVar(&czas, "t", 0, "czas podstawowy")
+	flag.IntVar(&czasplus, "p", 0, "czas nisko naliczany")
 
 	filename := flag.Args()[0]
 
-	
 }
